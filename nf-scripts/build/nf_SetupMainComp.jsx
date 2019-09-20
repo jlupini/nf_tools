76bbd1239e2dfe96241d7b114c39484de0ced2d4
Jesse Lupini <jlupini@gmail.com>
2019-09-20 13:52:52 -0700

    Better error messages in setupMainComp

--- nf-scripts/build/nf_SetupMainComp.jsx
+++ nf-scripts/build/nf_SetupMainComp.jsx
@@ -1,103 +1,81 @@
-var backdropFile, backdropFileName, backdropLayer, bgLayer, currentMarker, currentTime, dotOverlayFile, dotOverlayFileName, dotOverlayLayer, duplicatedFootageLayer, fadeLayer, fadeOpacity, fileName, footageFile, footageLayer, footageLayerName, gsPreset, i, j, mainComp, mainCompName, markerCount, markerStream, newComp, newCompLayer, newCompName, newComps, partsFolder, path, precomposedFootageLayer, prevTime, ref, rootFolder;
+var setupMainComp;
 
 $.evalFile(File($.fileName).path + "/runtimeLibraries.jsx");
 
+setupMainComp = function() {
+  var backdropFile, backdropFileName, backdropLayer, bgLayer, currentMarker, currentTime, dotOverlayFile, dotOverlayFileName, dotOverlayLayer, duplicatedFootageLayer, fadeLayer, fadeOpacity, fileName, footageFile, footageLayer, footageLayerName, gsPreset, i, j, mainComp, mainCompName, markerCount, markerStream, newComp, newCompLayer, newCompName, newComps, partsFolder, path, precomposedFootageLayer, prevTime, ref, rootFolder;
+  fileName = decodeURIComponent(app.project.file.name);
+  mainCompName = fileName.substr(0, fileName.indexOf('.')) + ' - MainComp';
+  footageFile = app.project.selection[0];
+  if (footageFile == null) {
+    return alert("Error\nNo footage file selected!");
+  }
+  backdropFileName = "nf-bg-v01.ai";
+  dotOverlayFileName = "particular-bg-overlay-v01.mov";
+  footageLayerName = "GREENSCREEN";
+  backdropFile = NFProject.findItem(backdropFileName);
+  dotOverlayFile = NFProject.findItem(dotOverlayFileName);
+  if (!((backdropFile != null) && (dotOverlayFile != null))) {
+    return alert("Error\nCan't find dependent files (backdrop and dot overlay) in the project.");
+  }
+  mainComp = app.project.items.addComp(mainCompName, 1920, 1080, 1.0, footageFile.duration, 29.9700012207031);
+  footageLayer = mainComp.layers.add(footageFile);
+  footageLayer.name = footageLayerName;
+  footageLayer.property('Transform').property("Scale").setValue([50, 50]);
+  markerStream = footageLayer.property('Marker');
+  markerCount = markerStream.numKeys;
+  bgLayer = mainComp.layers.addSolid([1, 1, 1], 'Background', 1920, 1080, 1);
+  bgLayer.moveBefore(footageLayer);
+  newComps = [];
+  prevTime = 0;
+  rootFolder = app.project.rootFolder;
+  partsFolder = app.project.items.addFolder('Parts');
+  for (i = j = 1, ref = markerCount + 1; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    duplicatedFootageLayer = footageLayer.duplicate();
+    duplicatedFootageLayer.name = footageLayerName;
+    duplicatedFootageLayer.inPoint = prevTime;
+    if (i === markerCount + 1) {
+      currentTime = duplicatedFootageLayer.outPoint = mainComp.duration;
+    } else {
+      currentMarker = markerStream.keyValue(i);
+      currentTime = duplicatedFootageLayer.outPoint = markerStream.keyTime(i);
+    }
+    newCompName = "Part " + i;
+    newComp = mainComp.layers.precompose([duplicatedFootageLayer.index], newCompName, true);
+    precomposedFootageLayer = newComp.layers[1];
+    path = Folder(File($.fileName).parent.parent.fsName).fsName + '/lib/NF Greenscreen Preset.ffx';
+    gsPreset = File(path);
+    precomposedFootageLayer.selected = true;
+    precomposedFootageLayer.applyPreset(gsPreset);
+    precomposedFootageLayer.selected = false;
+    backdropLayer = newComp.layers.add(backdropFile);
+    dotOverlayLayer = newComp.layers.add(dotOverlayFile);
+    backdropLayer.name = "NF Backdrop";
+    dotOverlayLayer.name = "Dot Overlay";
+    dotOverlayLayer.blendingMode = BlendingMode.SCREEN;
+    dotOverlayLayer.moveAfter(precomposedFootageLayer);
+    backdropLayer.moveAfter(dotOverlayLayer);
+    newComps.push(newComp);
+    newComp.parentFolder = partsFolder;
+    newCompLayer = mainComp.layers.byName(newCompName);
+    if (markerCount !== 0) {
+      newCompLayer.inPoint = prevTime - 3;
+      newCompLayer.outPoint = currentTime + 10;
+    }
+    newCompLayer.audioEnabled = false;
+    newComp.bgColor = [1, 1, 1];
+    newCompLayer.moveToBeginning();
+    prevTime = currentTime;
+  }
+  fadeLayer = mainComp.layers.addSolid([1, 1, 1], 'Fade In/Out', 1920, 1080, 1);
+  fadeOpacity = fadeLayer.property('Transform').property('Opacity');
+  fadeOpacity.setValuesAtTimes([0, 1, mainComp.duration - 2.5, mainComp.duration], [100, 0, 0, 100]);
+  fadeLayer.moveToBeginning();
+  return footageLayer.remove();
+};
+
 app.beginUndoGroup('Setup Main Comp');
 
-fileName = decodeURIComponent(app.project.file.name);
-
-mainCompName = fileName.substr(0, fileName.indexOf('.')) + ' - MainComp';
-
-footageFile = app.project.selection[0];
-
-if (footageFile == null) {
-  throw new Error("No footage file selected!");
-}
-
-backdropFileName = "nf-bg-v01.ai";
-
-dotOverlayFileName = "particular-bg-overlay-v01.mov";
-
-footageLayerName = "GREENSCREEN";
-
-backdropFile = NFProject.findItem(backdropFileName);
-
-dotOverlayFile = NFProject.findItem(dotOverlayFileName);
-
-if (!((backdropFile != null) && (dotOverlayFile != null))) {
-  throw new Error("Can't find dependent files in the project");
-}
-
-mainComp = app.project.items.addComp(mainCompName, 1920, 1080, 1.0, footageFile.duration, 29.9700012207031);
-
-footageLayer = mainComp.layers.add(footageFile);
-
-footageLayer.name = footageLayerName;
-
-footageLayer.property('Transform').property("Scale").setValue([50, 50]);
-
-markerStream = footageLayer.property('Marker');
-
-markerCount = markerStream.numKeys;
-
-bgLayer = mainComp.layers.addSolid([1, 1, 1], 'Background', 1920, 1080, 1);
-
-bgLayer.moveBefore(footageLayer);
-
-newComps = [];
-
-prevTime = 0;
-
-rootFolder = app.project.rootFolder;
-
-partsFolder = app.project.items.addFolder('Parts');
-
-for (i = j = 1, ref = markerCount + 1; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
-  duplicatedFootageLayer = footageLayer.duplicate();
-  duplicatedFootageLayer.name = footageLayerName;
-  duplicatedFootageLayer.inPoint = prevTime;
-  if (i === markerCount + 1) {
-    currentTime = duplicatedFootageLayer.outPoint = mainComp.duration;
-  } else {
-    currentMarker = markerStream.keyValue(i);
-    currentTime = duplicatedFootageLayer.outPoint = markerStream.keyTime(i);
-  }
-  newCompName = "Part " + i;
-  newComp = mainComp.layers.precompose([duplicatedFootageLayer.index], newCompName, true);
-  precomposedFootageLayer = newComp.layers[1];
-  path = Folder(File($.fileName).parent.parent.fsName).fsName + '/lib/NF Greenscreen Preset.ffx';
-  gsPreset = File(path);
-  precomposedFootageLayer.selected = true;
-  precomposedFootageLayer.applyPreset(gsPreset);
-  precomposedFootageLayer.selected = false;
-  backdropLayer = newComp.layers.add(backdropFile);
-  dotOverlayLayer = newComp.layers.add(dotOverlayFile);
-  backdropLayer.name = "NF Backdrop";
-  dotOverlayLayer.name = "Dot Overlay";
-  dotOverlayLayer.blendingMode = BlendingMode.SCREEN;
-  dotOverlayLayer.moveAfter(precomposedFootageLayer);
-  backdropLayer.moveAfter(dotOverlayLayer);
-  newComps.push(newComp);
-  newComp.parentFolder = partsFolder;
-  newCompLayer = mainComp.layers.byName(newCompName);
-  if (markerCount !== 0) {
-    newCompLayer.inPoint = prevTime - 3;
-    newCompLayer.outPoint = currentTime + 10;
-  }
-  newCompLayer.audioEnabled = false;
-  newComp.bgColor = [1, 1, 1];
-  newCompLayer.moveToBeginning();
-  prevTime = currentTime;
-}
-
-fadeLayer = mainComp.layers.addSolid([1, 1, 1], 'Fade In/Out', 1920, 1080, 1);
-
-fadeOpacity = fadeLayer.property('Transform').property('Opacity');
-
-fadeOpacity.setValuesAtTimes([0, 1, mainComp.duration - 2.5, mainComp.duration], [100, 0, 0, 100]);
-
-fadeLayer.moveToBeginning();
-
-footageLayer.remove();
+setupMainComp();
 
 app.endUndoGroup();
--- nf-scripts/src/ae_scripts/SetupMainComp.coffee
+++ nf-scripts/src/ae_scripts/SetupMainComp.coffee
@@ -1,91 +1,94 @@
 $.evalFile File($.fileName).path + "/runtimeLibraries.jsx"
 
+setupMainComp = ->
+  fileName = decodeURIComponent app.project.file.name
+  mainCompName = fileName.substr(0, fileName.indexOf('.')) + ' - MainComp'
+  footageFile = app.project.selection[0]
+  return alert "Error\nNo footage file selected!" unless footageFile?
+
+  # Check that all the other assets we need exist
+  backdropFileName = "nf-bg-v01.ai"
+  dotOverlayFileName = "particular-bg-overlay-v01.mov"
+  footageLayerName = "GREENSCREEN"
+  backdropFile = NFProject.findItem backdropFileName
+  dotOverlayFile = NFProject.findItem dotOverlayFileName
+  return alert "Error\nCan't find dependent files (backdrop and dot overlay) in the project." unless backdropFile? and dotOverlayFile?
+
+  # Make the main comp and add footage
+  mainComp = app.project.items.addComp(mainCompName, 1920, 1080, 1.0, footageFile.duration, 29.9700012207031)
+  footageLayer = mainComp.layers.add footageFile
+  footageLayer.name = footageLayerName
+  footageLayer.property('Transform').property("Scale").setValue [50, 50]
+
+  # Get number of markers on layer
+  markerStream = footageLayer.property('Marker')
+  markerCount = markerStream.numKeys
+
+  # create new background layer
+  bgLayer = mainComp.layers.addSolid([1,1,1], 'Background', 1920, 1080, 1)
+  bgLayer.moveBefore footageLayer
+
+  # Populate the Parts
+  newComps = []
+  prevTime = 0
+  # Make a folder for the new Precomps
+  rootFolder = app.project.rootFolder
+  partsFolder = app.project.items.addFolder('Parts')
+
+  # For each marker, duplicate the audio/video layer, set in and out points, then precompose
+  for i in [1..markerCount+1]
+    duplicatedFootageLayer = footageLayer.duplicate()
+    duplicatedFootageLayer.name = footageLayerName
+    duplicatedFootageLayer.inPoint = prevTime
+
+    if i is markerCount + 1
+      currentTime = duplicatedFootageLayer.outPoint = mainComp.duration
+    else
+      currentMarker = markerStream.keyValue i
+      currentTime = duplicatedFootageLayer.outPoint = markerStream.keyTime i
+
+    newCompName = "Part #{i}"
+    newComp = mainComp.layers.precompose [duplicatedFootageLayer.index], newCompName, true
+    precomposedFootageLayer = newComp.layers[1]
+
+    # Apply the Animation Preset. NOTE: because of an AE bug, a layer has to be selected to apply a preset. Hence the hack
+    path = Folder(File($.fileName).parent.parent.fsName).fsName + '/lib/NF Greenscreen Preset.ffx'
+    gsPreset = File path
+    precomposedFootageLayer.selected = yes
+    precomposedFootageLayer.applyPreset gsPreset
+    precomposedFootageLayer.selected = no
+
+    backdropLayer = newComp.layers.add backdropFile
+    dotOverlayLayer = newComp.layers.add dotOverlayFile
+    backdropLayer.name = "NF Backdrop"
+    dotOverlayLayer.name = "Dot Overlay"
+    dotOverlayLayer.blendingMode = BlendingMode.SCREEN
+
+    dotOverlayLayer.moveAfter precomposedFootageLayer
+    backdropLayer.moveAfter dotOverlayLayer
+
+    newComps.push newComp
+    newComp.parentFolder = partsFolder
+
+    newCompLayer = mainComp.layers.byName(newCompName)
+    unless markerCount is 0
+      newCompLayer.inPoint = prevTime - 3
+      newCompLayer.outPoint = currentTime + 10
+
+    newCompLayer.audioEnabled = no
+    newComp.bgColor = [1,1,1]
+    newCompLayer.moveToBeginning()
+    prevTime = currentTime
+
+  fadeLayer = mainComp.layers.addSolid([1,1,1], 'Fade In/Out', 1920, 1080, 1)
+  fadeOpacity = fadeLayer.property('Transform').property('Opacity')
+  fadeOpacity.setValuesAtTimes [0, 1, mainComp.duration - 2.5, mainComp.duration], [100, 0, 0, 100]
+  fadeLayer.moveToBeginning()
+
+  footageLayer.remove()
+
 app.beginUndoGroup 'Setup Main Comp'
 
-fileName = decodeURIComponent app.project.file.name
-mainCompName = fileName.substr(0, fileName.indexOf('.')) + ' - MainComp'
-footageFile = app.project.selection[0]
-throw new Error "No footage file selected!" unless footageFile?
-
-# Check that all the other assets we need exist
-backdropFileName = "nf-bg-v01.ai"
-dotOverlayFileName = "particular-bg-overlay-v01.mov"
-footageLayerName = "GREENSCREEN"
-backdropFile = NFProject.findItem backdropFileName
-dotOverlayFile = NFProject.findItem dotOverlayFileName
-throw new Error "Can't find dependent files in the project" unless backdropFile? and dotOverlayFile?
-
-# Make the main comp and add footage
-mainComp = app.project.items.addComp(mainCompName, 1920, 1080, 1.0, footageFile.duration, 29.9700012207031)
-footageLayer = mainComp.layers.add footageFile
-footageLayer.name = footageLayerName
-footageLayer.property('Transform').property("Scale").setValue [50, 50]
-
-# Get number of markers on layer
-markerStream = footageLayer.property('Marker')
-markerCount = markerStream.numKeys
-
-# create new background layer
-bgLayer = mainComp.layers.addSolid([1,1,1], 'Background', 1920, 1080, 1)
-bgLayer.moveBefore footageLayer
-
-# Populate the Parts
-newComps = []
-prevTime = 0
-# Make a folder for the new Precomps
-rootFolder = app.project.rootFolder
-partsFolder = app.project.items.addFolder('Parts')
-
-# For each marker, duplicate the audio/video layer, set in and out points, then precompose
-for i in [1..markerCount+1]
-  duplicatedFootageLayer = footageLayer.duplicate()
-  duplicatedFootageLayer.name = footageLayerName
-  duplicatedFootageLayer.inPoint = prevTime
-
-  if i is markerCount + 1
-    currentTime = duplicatedFootageLayer.outPoint = mainComp.duration
-  else
-    currentMarker = markerStream.keyValue i
-    currentTime = duplicatedFootageLayer.outPoint = markerStream.keyTime i
-
-  newCompName = "Part #{i}"
-  newComp = mainComp.layers.precompose [duplicatedFootageLayer.index], newCompName, true
-  precomposedFootageLayer = newComp.layers[1]
-
-  # Apply the Animation Preset. NOTE: because of an AE bug, a layer has to be selected to apply a preset. Hence the hack
-  path = Folder(File($.fileName).parent.parent.fsName).fsName + '/lib/NF Greenscreen Preset.ffx'
-  gsPreset = File path
-  precomposedFootageLayer.selected = yes
-  precomposedFootageLayer.applyPreset gsPreset
-  precomposedFootageLayer.selected = no
-
-  backdropLayer = newComp.layers.add backdropFile
-  dotOverlayLayer = newComp.layers.add dotOverlayFile
-  backdropLayer.name = "NF Backdrop"
-  dotOverlayLayer.name = "Dot Overlay"
-  dotOverlayLayer.blendingMode = BlendingMode.SCREEN
-
-  dotOverlayLayer.moveAfter precomposedFootageLayer
-  backdropLayer.moveAfter dotOverlayLayer
-
-  newComps.push newComp
-  newComp.parentFolder = partsFolder
-
-  newCompLayer = mainComp.layers.byName(newCompName)
-  unless markerCount is 0
-    newCompLayer.inPoint = prevTime - 3
-    newCompLayer.outPoint = currentTime + 10
-
-  newCompLayer.audioEnabled = no
-  newComp.bgColor = [1,1,1]
-  newCompLayer.moveToBeginning()
-  prevTime = currentTime
-
-fadeLayer = mainComp.layers.addSolid([1,1,1], 'Fade In/Out', 1920, 1080, 1)
-fadeOpacity = fadeLayer.property('Transform').property('Opacity')
-fadeOpacity.setValuesAtTimes [0, 1, mainComp.duration - 2.5, mainComp.duration], [100, 0, 0, 100]
-fadeLayer.moveToBeginning()
-
-footageLayer.remove()
+setupMainComp()
 
 app.endUndoGroup()
